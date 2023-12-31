1071748d696c78d281639e40b2149f27d37cb2b4
Chaiwat Suttipongsakul <cwt@bashell.com>
2023-12-31 19:52:49 +0700

    Update kernel-update.sh

--- kernel-update.sh
+++ kernel-update.sh
@@ -10,6 +10,5 @@ cd ${IMG}-${SDK}-${REL}
 wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-515-starfive-vf2-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
 wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-515-starfive-vf2-headers-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
 wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-515-starfive-vf2-soft_3rdpart-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
-#sudo pacman -U *.tar.zst
-#sudo reboot
-
+sudo pacman -U *.tar.zst
+sudo reboot
