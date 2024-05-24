756be8494893662ea6a78a5f3b5ef2d9e4d7ba94
Chaiwat Suttipongsakul <cwt@bashell.com>
2024-05-24 21:21:53 +0700

    update kernel update script

--- kernel-update.sh
+++ kernel-update.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-IMG=cwt21
-SDK=5.11.3
+IMG=cwt22
+SDK=5.12.0
 REL=2
 
 mkdir ${IMG}-${SDK}-${REL}
 cd ${IMG}-${SDK}-${REL}
 
-wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-515-starfive-vf2-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
-wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-515-starfive-vf2-headers-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
-wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-515-starfive-vf2-soft_3rdpart-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
+wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-6.6-starfive-vf2-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
+wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-6.6-starfive-vf2-headers-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
+wget https://github.com/cwt-vf2/linux-cwt-starfive-vf2/releases/download/${IMG}-${SDK}-${REL}/linux-cwt-6.6-starfive-vf2-soft_3rdpart-${IMG:3}.${SDK}-${REL}-riscv64.pkg.tar.zst
 sudo pacman -U *.tar.zst
-sudo reboot
+echo "reboot your board to use the new kernel"
